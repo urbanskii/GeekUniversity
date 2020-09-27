f3c6c289e66be77eb47a1246ba45b9f0975eb650
urbanskii <marcos.urbanski@gmail.com>
2020-09-27 14:15:19 -0300

    Change the file section03.c include comments about the exercise.

--- section03.c
+++ section03.c
@@ -1,3 +1,14 @@
+/* Author: Marcos Antonio Urbanski 27/09/2020
+ * This is a exercise from Udemy course "algoritmos-e-logica-de-programacao-essencial" by GeekUniversity.
+ * This program was made to calc the average stock for the parts,
+ * The variables below, receive the data of the user,
+ * Minimum_quantity receive the minimum quantity of parts in the stocks,
+ * Max_quantity receive the maximal quantity of parts in the stock.
+ * average_stock calc the average stock of parts,
+ * adding the variables minimum_quantity and max_quantity divinding per 2.
+ * The result of calc is display in the output with the function printf.
+ *
+ */
 #include <stdio.h>
 
 int main(){
