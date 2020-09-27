ea2d66d43352c5e03a75590225b52f8b2096a815
urbanskii <marcos.urbanski@gmail.com>
2020-09-27 15:32:21 -0300

    close #1, close issue section02.c

--- section02.c
+++ section02.c
@@ -1,3 +1,12 @@
+/* Author: Marcos Antonio Urbanski 27/09/2020
+ * This is a exercise from Udemy course "algoritmos-e-logica-de-programacao-essencial" by GeekUniversity.
+ * This program was made to adding variables and multiply,
+ * The variables below, receive the data of the user,
+ * num1, num2 receive the integer numbers.
+ * variable multiplication receive the result of multiplication of variable sum and num1
+ * The result of multiplication is display in the output with the function printf.
+ *
+ */
 #include <stdio.h>
 // std = Standard
 // io = input/output
